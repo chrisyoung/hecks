acacb0eef9de2d89b749b5b59ff65a2061cd0128
Chris Young <chris@trusona.com>
2026-03-29 10:16:07 -0700

    Regenerate all examples + add hecks regenerate_examples command
    Freshly generated: pizzas_domain, pizzas_static_ruby, pizzas_static_go,
    pizzas_rails. New CLI command regenerates all four from the pizzas domain.

---
+++ examples/pizzas/pizzas_static_go/domain/pizza_by_description_query.go
@@ -0,0 +1,13 @@
+package domain
+
+func PizzaByDescription(repo PizzaRepository, desc string) ([]*Pizza, error) {
+	all, err := repo.All()
+	if err != nil { return nil, err }
+	var results []*Pizza
+	for _, item := range all {
+		if item.Description == desc {
+			results = append(results, item)
+		}
+	}
+	return results, nil
+}
